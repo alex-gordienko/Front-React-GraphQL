9740260df477ce93dfe25da71d570f341b3a9a35
AleXoiD <alexoid1999@AleXoiD-ubuntu>
2021-02-03 19:44:09 +0200

    Updates

---
+++ precache-manifest.70e162d2c071b03a7d7130c0ddc46d37.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55154f0fa95ac0352a0385fa2f67b83f",
+    "url": "/Front-React-GraphQL/index.html"
+  },
+  {
+    "revision": "90f75b5f073eb743252d",
+    "url": "/Front-React-GraphQL/static/css/main.4b697732.chunk.css"
+  },
+  {
+    "revision": "702edcf893164e68388a",
+    "url": "/Front-React-GraphQL/static/js/2.ea7f1086.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/Front-React-GraphQL/static/js/2.ea7f1086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f75b5f073eb743252d",
+    "url": "/Front-React-GraphQL/static/js/main.ca877cba.chunk.js"
+  },
+  {
+    "revision": "18b2e1529c62a4e609db",
+    "url": "/Front-React-GraphQL/static/js/runtime-main.8f6d103f.js"
+  },
+  {
+    "revision": "f3c3a424565c9ac435498f36f989ce8d",
+    "url": "/Front-React-GraphQL/static/media/Group 845.f3c3a424.svg"
+  },
+  {
+    "revision": "f50dd28a7493356d91aafce09981f90b",
+    "url": "/Front-React-GraphQL/static/media/Group 846.f50dd28a.svg"
+  },
+  {
+    "revision": "38905ef72dfd234f2e0730083e7dbcfb",
+    "url": "/Front-React-GraphQL/static/media/more.38905ef7.svg"
+  }
+]);
